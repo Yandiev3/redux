c7e3b72db11518078789ee26c39a898cf0cc56e5
yandiev3 <yandiev3@mail.ru>
2024-12-09 15:57:02 +0300

    redux09.12

--- src/store/customers.js
+++ src/store/customers.js
@@ -7,7 +7,5 @@ export const fetchCustomers = ()=>{
         fetch("https://671f75cfe7a5792f052e55dd.mockapi.io/items")
         .then(response => response.json())
         .then(json => dispatch(addManyCustomersAction(json)))
-        .catch(err => console.log(err)
-        )
-    }
+    } 
 }
